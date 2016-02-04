32e5414bbf0a01482db090e87becae108c277779
AleahEllis <aleah.is@icloud.com>
2016-02-04 13:58:16 -0500

    without bonus

---
+++ lab5.js
@@ -0,0 +1,9 @@
+var temp=5.0;
+var ftoC=(temp - 32.0) * (5/9);
+console.log("The tempurature converted into Celcius is "+ftoC+" degrees Celcius");
+
+console.log(ftoC);
+
+var ctoF=ftoC* (9/5) +32.0;
+console.log("The tempurature converted into F is "+ctoF+" degrees F");
+
